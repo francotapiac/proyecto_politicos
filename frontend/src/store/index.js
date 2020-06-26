7927fdb218b4efde4af989c317b0854905e1940b
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-06-26 18:37:12 -0400

    se crea nuevo frontend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import layout from './layout';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
-})
+    layout,
+  },
+});
