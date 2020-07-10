b7e382c77835af155ebf6f92538dbac876f3d356
Franco <franco.tapia.c@usach.cl>
2020-07-09 21:41:41 -0400

    Implementación de gráfico en politico

--- frontend/src/views/PoliticalsApproval.vue
+++ frontend/src/views/PoliticalsApproval.vue
@@ -1,10 +1,15 @@
 <template>
-    
+    <v-container>
+        <specificChart/>
+    </v-container>
 </template>
 
 <script>
+import SpecificChart from '@/components/politicals/specificChart.vue';
 export default {
-    
+    components: {
+      SpecificChart,
+    },
 }
 </script>
 
