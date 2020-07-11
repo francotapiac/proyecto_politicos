1f53bc358d9f0df3544a85a89f7b9eb1d706a406
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-07-11 18:08:06 -0400

    se agrega el sentiment Analyzer: contiene error

---
+++ back/src/main/java/com/project/back/sentimentAnalysis/SentimentAnalyzer.java
@@ -0,0 +1,30 @@
+package com.project.back.sentimentAnalysis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class SentimentAnalyzer {
+
+    //Atributes
+    @Autowired
+    private SentimentClassifier sentimentClassifier;
+
+    //Methods
+    public HashMap<String, Double> getClasification(String text){
+        sentimentClassifier = new SentimentClassifier();
+        return this.sentimentClassifier.classify(text);
+    }
+
+    //Getters
+    public SentimentClassifier getClassifier() {
+        return sentimentClassifier;
+    }
+
+    //Setters
+    public void setClassifier(SentimentClassifier classifier) {
+        this.sentimentClassifier = classifier;
+    }
+}
